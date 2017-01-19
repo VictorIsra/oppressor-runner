f88e4cb3b9a5125759940dd4fbbab59c943135cb
Victor Israel <victor93@globo.com>
2017-01-19 19:55:35 -0200

    algumas alterações em cria_oprimido() e variavel para controle de fim de jogo adicionada

--- js/game.js
+++ js/game.js
@@ -1,86 +1,114 @@
 
-//pegando os atributos do jogo: dimensoes da tela, dos personagens etc
-screen_width = document.getElementById("background").clientWidth;//client_qqrCoisa para só pegar o valor do padding ( ignorar as bordas, margens etc)
-screen_heigth = document.getElementById("background").clientHeight;
-player_heigth = document.getElementById("player").clientHeight;
-gatilho = document.getElementsByTagName("body")[0];//usado para saber quando o jogo efetivamente começará
-//atributos do personagem:
-player = document.getElementById("player");
-pos_inicial = screen_heigth - player_heigth;
-player_pos = solo_pos = pos_inicial;//posicao inicial do personagem
-player.style.top = player_pos + 'px';//coloca o personagem na posicao inicial
-no_chao = true;//para saber se o personagem está pulando ou no solo
+(function(){
+    //pegando os atributos do jogo: dimensoes da tela, dos personagens etc
+    var screen_width = document.getElementById("background").clientWidth;//client_qqrCoisa para só pegar o valor do padding ( ignorar as bordas, margens etc)
+    var screen_heigth = document.getElementById("background").clientHeight;
+    console.log("width tela",screen_width, "height tela", screen_heigth);
+    var gatilho = document.getElementsByTagName("body")[0];//usado para saber quando o jogo efetivamente começará
+    var fim_jogo = false;//acionará certos eventos quando verdadeiro
+    //atributos do personagem:
+    var player = document.getElementById("player");
+    var player_width = player.clientWidth;
+    var player_heigth = player.clientHeight;
+    var pos_inicial_y = screen_heigth - player_heigth;//calcula posicao vertical inicial do personagem em relacao ao solo
+    var player_pos_y = pos_inicial_y;//posicao vertical inicial do personagem
+    var player_pos_x = 0;//posicao horizontal inicial do personagem
+    var no_chao = true;//para saber se o personagem está pulando ou no solo
+    player.style.top = player_pos_y + 'px';//coloca o personagem na posicao vertical inicial
+    
+    gatilho.addEventListener("keydown", comeca_jogo);//quando o personagem pular pela primeira vez, o jogo começará efetivamente
+    document.addEventListener("keydown", pula);
 
-gatilho.addEventListener("keydown", comeca_jogo);//quando o personagem pular pela primeira vez, o jogo começará efetivamente
-document.addEventListener("keydown", pula);
-
-function pula(tecla){
-	if(tecla.keyCode == 32 && no_chao ){//só pulará se estiver no chão ( e apertado espaço
-        window.no_chao = false;
-        var pulo = setInterval(function(){  
-            window.player_pos-=20;
-            player.style.top = player_pos + 'px';
-            if(player_pos < screen_heigth/2){
-                clearInterval(pulo);
-                desce();
-            }
-        }, 30)
-	}
-}
-
-function desce(){
-	if(!no_chao){
-        var pulo = setInterval(function(){
-            window.player_pos+=20;
-            player.style.top = window.player_pos + 'px';
-            if(window.player_pos >= window.pos_inicial){
-                clearInterval(pulo);
-                window.no_chao=true;
-            }
-        }, 30)
+    function pula(tecla){
+    	if(tecla.keyCode == 32 && no_chao && !fim_jogo ){//só pulará se estiver no chão ( e apertado espaço
+            no_chao = false;
+            var pulo = setInterval(function(){
+                var velocidade = 30;  
+                player_pos_y-= velocidade;
+                //console.log("posicao Y do player  " + player_pos_y);
+                player.style.top = player_pos_y + 'px';
+                if(player_pos_y < screen_heigth/2){
+                    clearInterval(pulo);
+                    desce();
+                }
+            }, 30)
+    	}
     }
-}
 
-function comeca_jogo(tecla){
-    if(tecla.keyCode == 32){
-        cria_oprimido();
-        gatilho.removeEventListener("keydown", comeca_jogo);
+    function desce(){
+    	if(!no_chao){
+            var pulo = setInterval(function(){
+                var velocidade = 30;
+                player_pos_y+= velocidade;
+                player.style.top = player_pos_y + 'px';
+                if(player_pos_y >= pos_inicial_y){
+                    clearInterval(pulo);
+                    no_chao=true;
+                }
+            }, 30)
+        }
     }
-}
-/*
-function intervalo(){  
-    var min = 500//750;//parseInt(Math.random()*(Math.random()*1000), 10);
-    var max = 4000//1500;parseInt(Math.random()*(Math.random()*1000), 10) + min;4
-    x = parseInt(Math.random()*(max - min) + min, 10);
-    console.log("velo " + x);
-    return x;
-}*/
-function cria_oprimido(){
-        (function(){
-            var oprimido = document.createElement("div");
-            oprimido.className = "oprimido";
-            oprimido.style.top = solo_pos + "px";
-            //oprimido = personaliza_oprimido();
-            document.getElementById("background").appendChild(oprimido);
-            var array = document.getElementsByClassName("oprimido");
-            console.log("quantidade de elementos no vetor de oprimidos criados: " + array.length);
-            var oprimido_pos = 0;
-            var corre_oprimido = setInterval(function(){
-                oprimido_pos +=5;
-                oprimido.style.right = oprimido_pos + "px";
-                if(oprimido_pos > screen_width){
-                    oprimido.parentNode.removeChild(oprimido);
-                    //background.removeChild(oprimido);
-                   //background.removeChild(array[0]);
-                   //array[0].remove();
-                    clearInterval(corre_oprimido);
+
+    function comeca_jogo(tecla){
+        if(tecla.keyCode == 32){
+            var anda = setInterval(function(){
+                //if(no_chao)//ver possivel "bug" depois caso queira manter descomentado "bug":se mantiver pressionado pode demorarar pra comecar
+                player_pos_x += 1;
+               // console.log("posicao X do player  " + player_pos_x);
+                player.style.left = player_pos_x + 'px';
+                if(player_pos_x >= screen_heigth/15){ //acrescentar "&& no_chao"?
+                    clearInterval(anda);
+                    cria_oprimido();
                 }
             },30);
-    })();
-    //setTimeout(cria_oprimido, intervalo);
-    setTimeout(cria_oprimido, 1000);//tempo arbitrário só pra ver as bads   
-}
+            gatilho.removeEventListener("keydown", comeca_jogo);
+        }
+    }
+    /*function intervalo(){  
+        var min = 500//750;//parseInt(Math.random()*(Math.random()*1000), 10);
+        var max = 4000//1500;parseInt(Math.random()*(Math.random()*1000), 10) + min;4
+        var x = parseInt(Math.random()*(max - min) + min, 10);
+        console.log("tempo " + x);
+        return x;
+    }*/
 
-function personaliza_oprimido(){
-    //fazer dps
-}
+    function cria_oprimido(){
+            (function(){
+                var oprimido = document.createElement("div");
+                oprimido.className = "oprimido";
+                //oprimido = personaliza_oprimido();
+                document.getElementById("background").appendChild(oprimido);
+                var oprimido_pos_x = screen_width - oprimido.clientWidth;//posicao horizontal inicial de um oprimido
+                var oprimido_pos_y = screen_heigth - oprimido.clientHeight;//posicao vertical inicial de um oprimido
+                oprimido.style.left = oprimido_pos_x + "px";//coloca o oprimido na posicao horizontal inicial
+                oprimido.style.top = oprimido_pos_y + "px";//coloca o oprimido na posicao vertical inicial
+                var count = 0;
+                var corre_oprimido = setInterval(function(){
+                    var velocidade = 15;
+                    console.log("posicao X oprimido ", oprimido_pos_x);
+                    oprimido_pos_x -= velocidade;
+                     oprimido.style.left = oprimido_pos_x + "px";
+                    //colide(oprimido);
+                    if(oprimido_pos_x <= -velocidade ){//depois ver como acertar isso ( só funciona se for multiplo de 15 o.o) -(oprimido_pos_x+velocidade) ou -velocidade e p essa resolucao -.-
+                        oprimido.parentNode.removeChild(oprimido);
+                        clearInterval(corre_oprimido);
+                    }
+                },30);
+        })();
+        var id = setTimeout(cria_oprimido, 1000);//intervalo());
+        if(fim_jogo)
+            clearInterval(id);
+    }
+
+    function personaliza_oprimido(){
+        //fazer dps
+    }
+
+    function colide(x){//*lembrar q nao tem passagem por ref mas dá pra alterar objetos*
+        //console.log("objeto: ",x, "left ", x.style.left);
+        
+    }
+
+})();
+
+
