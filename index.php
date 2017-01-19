3fd0ffb36282ea37961987d9ad9d2f226638b6f9
Victor Israel <victor93@globo.com>
2017-01-19 18:46:36 -0200

    começando jogo

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Opressor runner</title>
+	<link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+<h1 class="texto">Opressor Runner
+	<h3 class="texto">Será que você aguenta ficar 1 minuto sem oprimir alguém?</h3>
+</h1>
+<div id="background">
+	<div id="player"></div>	
+	<div id="placar">0</div>
+	<!--<div id="mostra_mensagem">Fim de jogo!<br>aperte 'ESC' para tentar novamente.</div>-->
+	<script src="js/game.js"></script> 
+
+</div>	
+<h3 class="texto">Aperte a barra de espaço para pular e começar a opressão</h3>
+	
+</body>
+</html>
---
+++ js/game.js
@@ -0,0 +1,41 @@
+
+//pegando os atributos do jogo: dimensoes da tela e dos personagens
+screen_width = document.getElementById("background").clientWidth;//client_qqrCoisa para só pegar o valor do padding ( ignorar as bordas, margens etc)
+screen_heigth = document.getElementById("background").clientHeight;
+player_heigth = document.getElementById("player").clientHeight;
+//atributos do personagem:
+player = document.getElementById("player");
+player_pos = solo_pos = screen_heigth - player_heigth;//posicao inicial do personagem
+player.style.top = player_pos + 'px';//coloca o personagem na posicao inicial
+no_chao = true;//para saber se o personagem está pulando ou no solo
+
+document.addEventListener("keydown", pula);
+//document.addEventListener("keydown", comeca_jogo);
+
+function pula(tecla){
+	if(tecla.keyCode == 32 && no_chao ){//só pulará se estiver no chão ( e apertado espaço )
+		id = setTimeout(function(){pula(tecla)}, 30);
+		if(player_pos < screen_heigth/2){//altura máxima do pulo será sempre a metade da altura da tela
+			clearInterval(id);
+			desce();//ao atingir altura máxima, o personagem começará a descer
+		}
+		else{
+			player_pos-=20;
+			player.style.top = player_pos + 'px';
+		}
+	}
+}
+
+function desce(){
+	no_chao = false;
+	id = setTimeout(function(){desce()}, 30);
+	if(player_pos >= solo_pos){//para que o personagem volte ao nível do solo depois de pular ( e nao caia pra baixo da tela)
+		clearInterval(id);
+		player.style.top = solo_pos + 'px';
+		no_chao = true;	
+	}
+	else{
+		player_pos+=20;
+		player.style.top = player_pos + 'px';
+	}
+}
