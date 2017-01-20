afafa7dd3f5a30c1107cd03939caa2510f36700f
Victor Israel <victor93@globo.com>
2017-01-20 13:04:52 -0200

    bug dos blocos corrigido e limpar_tela(), controla_teclado() adicionados

--- js/game.js
+++ js/game.js
@@ -1,9 +1,12 @@
-(function novo_jogo(){
+(function(){
     //pegando os atributos do jogo: dimensoes da tela, dos personagens etc
     var screen_width = document.getElementById("background").clientWidth;//client_qqrCoisa para só pegar o valor do padding ( ignorar as bordas, margens etc)
     var screen_heigth = document.getElementById("background").clientHeight;
     var gatilho = document.getElementsByTagName("body")[0];//usado para saber quando o jogo efetivamente começará
     var fim_jogo = false;//acionará certos eventos quando verdadeiro
+    var jogo_iniciado = false;
+    var fluxo;//controlará a chamada da instancialização dos oprimidos
+    var limpa_oprimido;//conterá os inimigos a serem limpados após o termino do jogo
     //atributos do personagem:
     var player = document.getElementById("player");
     var player_width = player.clientWidth;
@@ -13,43 +16,76 @@
     var player_pos_x = 0;//posicao horizontal inicial do personagem
     var no_chao = true;//para saber se o personagem está pulando ou no solo
     player.style.top = player_pos_y + 'px';//coloca o personagem na posicao vertical inicial
+    
+    
+    gatilho.addEventListener("keydown", controla_teclado);
+    
+    function controla_teclado(tecla){
+        switch (tecla.keyCode) {
+            case 32:
+                 if(jogo_iniciado)
+                    pula();
+                    
+                 else{
+                    pula();
+                    comeca_jogo();
+                    jogo_iniciado = true;
+                  }
+                  break;
 
-    gatilho.addEventListener("keydown", comeca_jogo);//quando o personagem pular pela primeira vez, o jogo começará efetivamente
-    document.addEventListener("keydown", pula);
-    document.addEventListener("keydown", restart_game);
-
-    function restart_game(tecla){
-        if(fim_jogo && tecla.keyCode == 27 ){
-            player.style.top  = pos_inicial_y;
-            player.style.left = 0;
-            novo_jogo();
-            //removeEventListener("keydown", comeca_jogo);
+            case 27:
+                if(fim_jogo){
+                    restart_game();
+                    fim_jogo = false;
+                    jogo_iniciado = false;
+                }
+                break;
         }
+
     }
-    function pula(tecla){
-    	if(tecla.keyCode == 32 && no_chao && !fim_jogo ){//só pulará se estiver no chão ( e apertado espaço
-            no_chao = false;
-            var pulo = setInterval(function(){
+    function comeca_jogo(){
+            var anda = setInterval(function(){
                 if(fim_jogo)
                     return;
-                var velocidade = 25;  
+                player_pos_x += 1;
+                player.style.left = player_pos_x + 'px';
+                if(player_pos_x >= screen_heigth/10){
+                    clearInterval(anda);
+                    fluxo = setInterval(cria_oprimido, 1000);//intervalo());
+                }
+            },30);
+    }
+
+    function restart_game(){
+            player.style.top  = pos_inicial_y;
+            player.style.left = 0;
+            player_pos_x = 0;
+            limpa_tela();
+    }
+
+    function pula(){
+        if(no_chao){
+            no_chao = false;
+            var pulo = setInterval(function(){
+                if(fim_jogo)//pra congelar o player caso ele estiver supindo após colidir
+                    return;
+                var velocidade = 20;  
                 player_pos_y-= velocidade;
-                //console.log("posicao Y do player  " + player_pos_y);
                 player.style.top = player_pos_y + 'px';
                 if(player_pos_y < screen_heigth/2){
                     clearInterval(pulo);
                     desce();
                 }
-            }, 30)
-    	}
+            }, 30);
+        }
     }
 
     function desce(){
-    	if(!no_chao && !fim_jogo){
+    	if(!no_chao){
             var pulo = setInterval(function(){
-                if(fim_jogo)
+                if(fim_jogo)//pra congelar o player caso ele estiver descendo após colidir
                     return;
-                var velocidade = 25;
+                var velocidade = 20;
                 player_pos_y+= velocidade;
                 player.style.top = player_pos_y + 'px';
                 if(player_pos_y >= pos_inicial_y){
@@ -60,63 +96,39 @@
         }
     }
 
-    function comeca_jogo(tecla){
-        if(tecla.keyCode == 32){
-            var anda = setInterval(function(){
-                player_pos_x += 1;
-               // console.log("posicao X do player  " + player_pos_x);
-                player.style.left = player_pos_x + 'px';
-                if(player_pos_x >= screen_heigth/10){
-                    clearInterval(anda);
-                    cria_oprimido();
-                }
-            },30);
-            gatilho.removeEventListener("keydown", comeca_jogo);
-        }
-    }
-    /*function intervalo(){  
-        var min = 500//750;//parseInt(Math.random()*(Math.random()*1000), 10);
-        var max = 4000//1500;parseInt(Math.random()*(Math.random()*1000), 10) + min;4
-        var x = parseInt(Math.random()*(max - min) + min, 10);
-        console.log("tempo " + x);
-        return x;
-    }*/
-
-    function cria_oprimido(){
-            (function(){
-                if(fim_jogo)
-                        return;
+    var cria_oprimido = function(){//pq seu fizesse ela se auto-invocar daria ruim?
+                /*if(fim_jogo)//sai da funcao
+                    return;*/
                 var oprimido = document.createElement("div");
                 oprimido.className = "oprimido";
                 //oprimido = personaliza_oprimido();
                 document.getElementById("background").appendChild(oprimido);
-                var array = document.getElementsByClassName("oprimido");
+                var oprimidos = document.getElementsByClassName("oprimido");//vetor com todos os oprimidos presentes na cena
                 var oprimido_pos_x = screen_width - oprimido.clientWidth;//posicao horizontal inicial de um oprimido
                 var oprimido_pos_y = screen_heigth - oprimido.clientHeight;//posicao vertical de um oprimido
                 oprimido.style.left = oprimido_pos_x + "px";//coloca o oprimido na posicao horizontal inicial
                 oprimido.style.top = oprimido_pos_y + "px";//coloca o oprimido na posicao vertical inicial
                 oprimido.style.width = oprimido.clientWidth + "px";
                 var corre_oprimido = setInterval(function(){
-                    if(fim_jogo)
+                    if(fim_jogo){//cessa o timer e sai da funcao
+                        clearInterval(corre_oprimido);
                         return;
-                    //console.log("ta dentro de corre oprimido ");
+                    }
                     var velocidade = 15;
                     oprimido_pos_x -= velocidade;
-                     oprimido.style.left = oprimido_pos_x + "px";
+                    oprimido.style.left = oprimido_pos_x + "px";
                     if(colidiu(oprimido, oprimido_pos_y)){
+                        limpa_oprimido = oprimidos;
+                        clearInterval(fluxo);//cessa a instancialização dos oprimidos
                         fim_jogo = true;
-                        var i;var tam = array.length;
-                        for(i<tam; i=0; i--)
-                            array[i].remove();
                     }
-                    if(oprimido_pos_x <= -velocidade ){
+                    if(oprimido_pos_x <= -velocidade ){//depois ver como acerta essa gambiarra
                         oprimido.parentNode.removeChild(oprimido);
                         clearInterval(corre_oprimido);
                     }
                 },30);
-            })();
-            var fluxo = setInterval(cria_oprimido, 1000);//intervalo());
-    }
+            };
+            
 
     function personaliza_oprimido(){
         //fazer dps
@@ -127,11 +139,18 @@
         var oprimido_pos_x    = parseInt(oprimidu.style.left)//posicao do vertice esquerdo inferior do oprimido
         var oprimido_pos_xD   = parseInt(oprimidu.style.left) + parseInt(oprimidu.style.width)//posicao do vertice direito inferior do oprimido
         var player_pos_xD     = player_pos_x + player_width//posicao do vertice direito do player
+        //console.log('player_pos_inf',player_pos_inf,'player_pos_y',player_pos_y,'player_pos_xD',player_pos_xD,'player_pos_x',player_pos_x);
         if(player_pos_inf >= opri_pos_y && ( (player_pos_xD >= oprimido_pos_x && player_pos_xD <= oprimido_pos_xD)
             || (player_pos_x >= oprimido_pos_x && player_pos_x <= oprimido_pos_xD) ))
                 return true;
     }
 
+    function limpa_tela(){
+        while(limpa_oprimido[0])//limpa os blocos da tela
+            limpa_oprimido[0].parentNode.removeChild(limpa_oprimido[0]);
+
+    }
+    console.log("lobo");
 })();
 
 
